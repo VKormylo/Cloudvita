53bcbb19bb41ba08fc8f137b41be31f13946f7cf
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-06-05 08:59:24 +0300

    Added translations for app

---
+++ Cloudvita-Client/src/constants/translations/uk/index.ts
@@ -0,0 +1,25 @@
+import common from './common.json'
+import forecast from './forecast.json'
+import header from './header.json'
+import weather from './weather.json'
+import sidebar from './sidebar.json'
+import conditions from './conditions.json'
+import auth from './auth.json'
+import locations from './locations.json'
+import settings from './settings.json'
+
+const uk = {
+  translations: {
+    common,
+    forecast,
+    header,
+    weather,
+    sidebar,
+    conditions,
+    auth,
+    locations,
+    settings
+  }
+}
+
+export default uk
